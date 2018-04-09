ae34532b88532fde4b21b45e81e329b75349c38e
Toshi Piazza <toshi.piazza@gmail.com>
2018-04-09 20:43:33 +0000

    Removes ok message payload

--- App.elm
+++ App.elm
@@ -8,9 +8,9 @@ import Navigation
 import Messages        exposing (..)
 
 
--- TODO: get websockets back to original functionality
 -- TODO: read uuid from query param?
 --    or read device id from query param to subscribe?
+-- TODO: handle subscription message
 
 
 type alias Model =
@@ -22,11 +22,10 @@ type alias Model =
 main : Program Never Model Msg
 main = program
     { init =
-        let uuid = "6be07c15-94ad-4d48-bcc5-09e2359d4ebe"
+        let uuid = "9f7b99e9-7e91-4d12-8527-1d782f8d03a6"
             -- don't worry too much about this, it's an nginx reverse proxy which sets the
             -- Access-Control-Allow-Origin: * header
-            url = "http://128.113.17.41:81/message?uuid=" ++ uuid
-            req = Http.get url messageOldDecoder in
+            url = "http://128.113.17.41:81/message?uuid=" ++ uuid in
         ({ uuid = uuid, messages = [ ] }, WebSocket.send "ws://128.113.17.41:81/ws" uuid)
     , view = view
     , update = update
@@ -40,7 +39,6 @@ type Msg
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    let openWS = WebSocket.send "ws://128.113.17.41:81/ws" model.uuid in
     case msg of
         NewMsg json ->
             case messagePushJetDecoder json of
@@ -53,13 +51,11 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ div [] (List.filterMap viewMessage model.messages) ]
-viewMessage : MessagePushJet -> Maybe (Html Msg)
+        [ div [] (List.map viewMessage model.messages) ]
+viewMessage : MessagePushJet -> Html Msg
 viewMessage msg =
     case msg of
-        MessageStatus  msg ->
-            Nothing
         MessagePayload msg ->
-            Just (div []
-                    [ a [ href msg.message.link ] [ text (msg.message.title ++ ": " ++ msg.message.message) ] ])
+            div []
+                [ a [ href msg.message.link ] [ text (msg.message.title ++ ": " ++ msg.message.message) ] ]
 -- vim:ft=haskell:
--- Messages.elm
+++ Messages.elm
@@ -2,7 +2,6 @@ module Messages exposing (..)
 
 import Json.Decode
 
-type alias MessageOkJson      = { status : String }
 type alias MessagePayloadJson = { message: MessagePayloadInnerJson }
 type alias MessagePayloadInnerJson =
     { title     : String
@@ -11,15 +10,8 @@ type alias MessagePayloadInnerJson =
     }
 
 type MessagePushJet
-    = MessageStatus  MessageOkJson
-    | MessagePayload MessagePayloadJson
+    = MessagePayload MessagePayloadJson
 
-messageOldDecoder : Json.Decode.Decoder (List MessagePayloadInnerJson)
-messageOldDecoder =
-        (Json.Decode.field "messages" (Json.Decode.list messagePayloadInnerDecoder))
-
-messageOKDecoder      =
-    Json.Decode.map  MessageOkJson      (Json.Decode.field "status"  Json.Decode.string)
 messagePayloadDecoder =
     Json.Decode.map  MessagePayloadJson (Json.Decode.field "message" messagePayloadInnerDecoder)
 messagePayloadInnerDecoder =
@@ -28,9 +20,7 @@ messagePayloadInnerDecoder =
                                              (Json.Decode.field "link"    Json.Decode.string)
 
 messagePushJetDecoder json =
-    case Json.Decode.decodeString messageOKDecoder json of
-        Ok ok -> Ok (MessageStatus ok)
-        Err _ -> case Json.Decode.decodeString messagePayloadDecoder json of
-            Ok ok -> Ok (MessagePayload ok)
-            Err _ -> Err "Could not parse JSON"
+    case Json.Decode.decodeString messagePayloadDecoder json of
+        Ok ok -> Ok (MessagePayload ok)
+        Err _ -> Err "Could not parse JSON"
 -- vim:ft=haskell:
