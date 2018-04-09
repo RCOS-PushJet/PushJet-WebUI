e30c1f7a225ad6989201f0d655f68ae86b2dce8f
Toshi Piazza <toshi.piazza@gmail.com>
2018-04-09 16:58:48 +0000

    Slight refactor
    - Changes the urls to be the RPI-internal PushJet endpoint we setup.
    - Message fetching has been verified to work
    - Slight syntactic cleanup

--- App.elm
+++ App.elm
@@ -7,7 +7,11 @@ import Navigation
 
 import Messages        exposing (..)
 
-pushJetWebSocket = "wss://api.pushjet.io/ws"
+-- TODO: get websockets back to original functionality
+-- TODO: read uuid from query param?
+--    or read device id from query param to subscribe?
+
+pushJetWebSocket = "ws://128.113.17.41:81/ws"
 
 type alias Model =
     { uuid     : String
@@ -17,11 +21,11 @@ type alias Model =
 main : Program Never Model Msg
 main = program
     { init =
-        let uuid = "2aac6626-b783-48d1-881e-fc0eb99659d1"
+        let uuid = "2ffa1bbb-2c73-4426-8c1f-c8e11ca861f7"
             -- don't worry too much about this, it's an nginx reverse proxy which sets the
             -- Access-Control-Allow-Origin: * header
-            url  = "http://ec2-34-217-64-134.us-west-2.compute.amazonaws.com/message?uuid=" ++ uuid
-            req  = Http.get url messageOldDecoder in
+            url = "http://128.113.17.41:81/message?uuid=" ++ uuid
+            req = Http.get url messageOldDecoder in
         ({ uuid = uuid, messages = [ ] }, (Http.send OldMsg req))
     , view = view
     , update = update
@@ -33,31 +37,32 @@ type Msg
     | NewMsg String
 
 update : Msg -> Model -> (Model, Cmd Msg)
-update msg { uuid, messages } =
-    let openWS = WebSocket.send pushJetWebSocket uuid in
+update msg model =
+    let openWS = WebSocket.send pushJetWebSocket model.uuid in
     case msg of
         OldMsg (Ok newMessages) ->
-            ({uuid = uuid, messages = messages ++ (List.map (MessagePayload << MessagePayloadJson) newMessages)}, openWS)
+            ({ model | messages = model.messages ++ (List.map (MessagePayload << MessagePayloadJson) newMessages)}, openWS)
         OldMsg (Err _)          ->
-            ({uuid = uuid, messages = messages }, openWS)
+            (model, openWS)
         NewMsg json ->
             case messagePushJetDecoder json of
                 Ok  msg ->
-                    ({uuid = uuid, messages = messages ++ [ msg ]}, Cmd.none)
+                    ({ model | messages = model.messages ++ [ msg ]}, Cmd.none)
                 Err msg ->
-                    ({uuid = uuid, messages = messages ++ [ MessageStatus (MessageOkJson msg) ]}, Cmd.none)
+                    (model, Cmd.none)
+
 
 view : Model -> Html Msg
 view model =
     div []
-        [ div [] (List.map viewMessage model.messages) ]
-
-viewMessage : MessagePushJet -> Html Msg
+        [ div [] (List.filterMap viewMessage model.messages) ]
+viewMessage : MessagePushJet -> Maybe (Html Msg)
 viewMessage msg =
     case msg of
         MessageStatus  msg ->
-            div [] [ text ("status: " ++ msg.status) ]
+            Nothing
         MessagePayload msg ->
-            div []
-                [ a [ href msg.message.link ] [ text (msg.message.title ++ ": " ++ msg.message.message) ] ]
+            Just (
+                div []
+                    [ a [ href msg.message.link ] [ text (msg.message.title ++ ": " ++ msg.message.message) ] ])
 -- vim:ft=haskell:
