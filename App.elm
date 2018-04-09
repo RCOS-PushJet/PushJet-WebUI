bcf99755abf36119cab8845fda84d588c35371e4
Toshi Piazza <toshi.piazza@gmail.com>
2018-04-09 20:09:14 +0000

    Removes unread message fetching code
    turns out a *correct* PushJet websockets client will give you back all the unread messages

--- App.elm
+++ App.elm
@@ -7,43 +7,41 @@ import Navigation
 
 import Messages        exposing (..)
 
+
 -- TODO: get websockets back to original functionality
 -- TODO: read uuid from query param?
 --    or read device id from query param to subscribe?
 
-pushJetWebSocket = "ws://128.113.17.41:81/ws"
 
 type alias Model =
     { uuid     : String
     , messages : List MessagePushJet
     }
 
+
 main : Program Never Model Msg
 main = program
     { init =
-        let uuid = "2ffa1bbb-2c73-4426-8c1f-c8e11ca861f7"
+        let uuid = "6be07c15-94ad-4d48-bcc5-09e2359d4ebe"
             -- don't worry too much about this, it's an nginx reverse proxy which sets the
             -- Access-Control-Allow-Origin: * header
             url = "http://128.113.17.41:81/message?uuid=" ++ uuid
             req = Http.get url messageOldDecoder in
-        ({ uuid = uuid, messages = [ ] }, (Http.send OldMsg req))
+        ({ uuid = uuid, messages = [ ] }, WebSocket.send "ws://128.113.17.41:81/ws" uuid)
     , view = view
     , update = update
-    , subscriptions = (\_ -> WebSocket.listen pushJetWebSocket NewMsg)
+    , subscriptions = (\_ -> WebSocket.listen "ws://128.113.17.41:81/ws" NewMsg)
     }
 
+
 type Msg
-    = OldMsg (Result Http.Error (List MessagePayloadInnerJson))
-    | NewMsg String
+    = NewMsg String
+
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    let openWS = WebSocket.send pushJetWebSocket model.uuid in
+    let openWS = WebSocket.send "ws://128.113.17.41:81/ws" model.uuid in
     case msg of
-        OldMsg (Ok newMessages) ->
-            ({ model | messages = model.messages ++ (List.map (MessagePayload << MessagePayloadJson) newMessages)}, openWS)
-        OldMsg (Err _)          ->
-            (model, openWS)
         NewMsg json ->
             case messagePushJetDecoder json of
                 Ok  msg ->
@@ -62,7 +60,6 @@ viewMessage msg =
         MessageStatus  msg ->
             Nothing
         MessagePayload msg ->
-            Just (
-                div []
+            Just (div []
                     [ a [ href msg.message.link ] [ text (msg.message.title ++ ": " ++ msg.message.message) ] ])
 -- vim:ft=haskell:
